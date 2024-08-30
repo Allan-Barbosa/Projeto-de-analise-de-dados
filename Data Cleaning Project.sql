89772635cfe725a7c0b46a45fddf9de59760dc72
Allan Barbosa <93217070+Allan-Barbosa@users.noreply.github.com>
2024-08-29 23:35:11 -0300

    Add files via upload

---
+++ Data Cleaning Project.sql
@@ -0,0 +1,148 @@
+-- Data Cleaning
+
+SELECT *
+FROM layoffs;
+
+-- 1. Remove Duplicates
+-- 2. Standardize the Data
+-- 3. Null Values or blank values
+-- 4. Remove Any Column or Rows
+
+# Criando outra tabela
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+SELECT *
+FROM layoffs_staging;
+
+# Inserindo os dados
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+# Numerando linhas duplicadas
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY company, industry, total_laid_off, percentage_laid_off, 'date') AS row_num
+FROM layoffs_staging
+ORDER BY row_num DESC;
+
+# Exibindo linhas duplicadas
+WITH duplicate_cte AS
+(SELECT *,
+ROW_NUMBER() OVER(PARTITION BY company, location, industry, total_laid_off, 
+percentage_laid_off, `date`, stage, 
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging)
+SELECT *
+FROM duplicate_cte
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging
+WHERE company = 'Casper';
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY company, location, industry, total_laid_off, 
+percentage_laid_off, `date`, stage, 
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+# Deletando linhas duplicadas
+DELETE
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+-- standardizing data
+
+SELECT DISTINCT(TRIM(company))
+FROM layoffs_staging2;
+
+# Removendo Espaços na String
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY 1;
+
+# Padronizando nome escrito de formas diferentes
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE '%Crypto%';
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM layoffs_staging2
+ORDER BY 1;
+
+# Removendo caractere incorreto inserido
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+SELECT `date`
+FROM layoffs_staging2;
+
+# Transformando o valor de string para data
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+# Alterando a coluna para o tipo date
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '') AND 
+t2.industry IS NOT NULL AND t2.industry != '';
+
+# Preenchendo campos NULL e vazios com a informação adquirida em outras linhas
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE (t1.industry IS NULL OR t1.industry = '') AND 
+t2.industry IS NOT NULL AND t2.industry != '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL AND percentage_laid_off IS NULL;
+
+# Deletando linhas com 2 colunas importantes sem valor
+DELETE 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL 
+AND percentage_laid_off IS NULL;
+
+# Deletando coluna que não será utilizada e foi criada apenas para encontrar linhas duplicadas
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+SELECT *
+FROM layoffs_staging2;
